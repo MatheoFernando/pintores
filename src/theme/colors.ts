ef8ed89db957a6c038edd455b1a22c03c3a7d590
jvieirafernando20@gmail.com <jvieirafernando20@gmail>
2024-08-21 11:26:31 +0100

    Initial commit
    Generated by create-expo-app 3.0.0.

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+              white: "#ffff",
+              black: "#0000"
+              gray: {
+                            700: "#292929",
+                            800: "#4B4B4B",
+                            900: "#8E8E8E"
+              },
+}
